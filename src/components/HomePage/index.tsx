d8bc5f936050ad10668e0675777e99c7de9c550a
Nelson Chinedu <egwuchinedu69@gmail.com>
2020-03-19 09:58:31 +0100

    import components to file

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,10 +1,24 @@
 import React, { FunctionComponent } from 'react';
 
 import HeroSection from 'src/components/HomePage/HeroSection';
+import SkillCategory from 'src/components/SkillCategory';
+import VerifiedExpert from 'src/components/VerfiedExpert';
+import Testimonials from 'src/components/Testimonials';
+import JoinUs from 'src/components/JoinUs';
+import Footer from 'src/components/Footer';
 
 const HomePage: FunctionComponent<{}> = () => {
   return (
-    <HeroSection />
+    <div>
+      <HeroSection />
+      <main>
+        <SkillCategory />
+        <VerifiedExpert />
+        <Testimonials />
+        <JoinUs />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
